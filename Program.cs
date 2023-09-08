a87d1b1a5eacf33c982699d6debc526a2caa96cd
kian <lordkian2000@gmail.com>
2023-09-09 00:23:21 +0200

    Added HandleUpdateAsync and HandlePollingErrorAsync

--- Program.cs
+++ Program.cs
@@ -4,4 +4,49 @@ using Telegram.Bot.Polling;
 using Telegram.Bot.Types;
 using Telegram.Bot.Types.Enums;
 
+Console.WriteLine("Please Enter Telegram Bot API");
+var botAPI = Console.ReadLine();
+
+var botClient = new TelegramBotClient(botAPI);
+var cts = new CancellationTokenSource();
+ReceiverOptions receiverOptions = new()
+{
+    AllowedUpdates = Array.Empty<UpdateType>()
+};
+
+botClient.StartReceiving(
+    updateHandler: HandleUpdateAsync,
+    pollingErrorHandler: HandlePollingErrorAsync,
+    receiverOptions: receiverOptions,
+    cancellationToken: cts.Token
+);
+
+async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update,
+    CancellationToken cancellationToken)
+{
+    if (update.Message is not { } message)
+        return;
+    if (message.Text is not { } messageText)
+        return;
+    var chatId = message.Chat.Id;
+    Message sentMessage = await botClient.SendTextMessageAsync(
+        chatId: chatId,
+        text: "Your chatID: " + chatId,
+        cancellationToken: cancellationToken);
+}
+
+Task HandlePollingErrorAsync(ITelegramBotClient botClient, Exception exception,
+    CancellationToken cancellationToken)
+{
+    var ErrorMessage = exception switch
+    {
+        ApiRequestException apiRequestException
+            => $"Telegram API Error:\n[{apiRequestException.ErrorCode}]\n{apiRequestException.Message}",
+        _ => exception.ToString()
+    };
+
+    Console.WriteLine(ErrorMessage);
+    return Task.CompletedTask;
+}
+
 Console.ReadKey();
